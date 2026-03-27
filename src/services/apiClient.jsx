2ac8b0afd6a75c9bbb454bc7cc84e0f8e1267cf0
Leonardo <leofronchi@outlook.com>
2026-03-27 13:29:47 -0300

    adicionando /api

--- src/services/apiClient.jsx
+++ src/services/apiClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "https://ic-backend-production-6479.up.railway.app",
+  baseURL: "https://ic-backend-production-6479.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
