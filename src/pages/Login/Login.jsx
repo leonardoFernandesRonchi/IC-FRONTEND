1f125306e68f048a578c382baa2ad307b4f5c91e
Leonardo <leofronchi@outlook.com>
2026-03-27 14:02:09 -0300

    mudando login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
         email: data.email,
         password: data.password,
       });
-      login();
+      await login();
       navigate("/dashboard");
     } catch (error) {
       console.error(error);
